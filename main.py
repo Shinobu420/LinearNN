49d1f8e07cac7b5263bcd41a4bd1dcfa7be7b91d
Shinobu420 <112190455+Shinobu420@users.noreply.github.com>
2026-06-19 20:04:13 +0200

    feat: training setup

---
+++ main.py
@@ -0,0 +1,48 @@
+import torch
+from torch import nn
+from torch.utils.data import DataLoader
+from torchvision import datasets
+from linearNN import neuralNetwork, transformData
+
+#amd specific
+torch.set_float32_matmul_precision('high')
+
+device = "cuda" if torch.cuda.is_available() else "cpu"
+
+
+## code runs only in the main process
+if __name__ == "__main__":
+    print(f"Using {device} device")
+    dataset = datasets.ImageFolder(root='./PetImages', transform=transformData)
+    dataset_loader= DataLoader(dataset, batch_size=512, shuffle=True, num_workers=6, pin_memory=True)
+
+    ## start compilation
+    model = neuralNetwork().to(device)
+    optimized_model = torch.compile(model)
+    ## end compilation
+
+    ## start training
+    ## loss function
+    loss = nn.CrossEntropyLoss()
+    ## optimizer updates weights based on loss
+    optimizer = torch.optim.Adam(optimized_model.parameters(), lr=0.001)
+    print ("Start training")
+    model.train()
+    epochs = 10
+    for epoch in range(epochs):
+        print(f"Epoch {epoch+1}/{epochs}")
+        for batch, (x,y) in enumerate(dataset_loader):
+            x,y = x.to(device), y.to(device)
+
+            pred = optimized_model(x)
+            loss_value = loss(pred, y)
+
+            optimizer.zero_grad()
+            loss_value.backward()
+            optimizer.step()
+
+            if batch % 10 == 0:
+                print(f"Batch {batch}/{len(dataset_loader)} - Loss: {loss_value.item():.4f}")
+
+    torch.save(optimized_model._orig_mod.state_dict(), "cat_dog_model.pth")
+    print("Model saved to cat_dog_model.pth!")
