86c34089ad19bda7a48d8674b13daa77671cd9d8
Shinobu420 <112190455+Shinobu420@users.noreply.github.com>
2026-06-21 12:34:26 +0200

    style: format linear_nn.py to comply with PEP 8

--- linear_nn.py
+++ linear_nn.py
@@ -2,14 +2,15 @@ import torch
 from torch import nn
 from torchvision.transforms import v2
 
-class neuralNetwork(nn.Module):
+
+class NeuralNetwork(nn.Module):
     def __init__(self):
         super().__init__()
         # flatten image to pixel row (1D "Tensor")
         self.flatten = nn.Flatten()
         self.linear_relu_stack = nn.Sequential(
             # 256*256*3 compressed down to 512 features
-            nn.Linear(256*256*3, 512),
+            nn.Linear(256 * 256 * 3, 512),
             nn.ReLU(),
             # hidden layer with 512 features
             nn.Linear(512, 512),
@@ -22,8 +23,8 @@ class neuralNetwork(nn.Module):
         x = self.flatten(x)
         return self.linear_relu_stack(x)
 
-transformData = v2.Compose([
-    #resize every image to 256x256
+transform_data = v2.Compose([
+    # resize every image to 256x256
     v2.Resize((256, 256)),
     v2.ToImage(),
     v2.ToDtype(torch.float32, scale=True)
